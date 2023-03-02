7ece58d5ea0199abd65d2763c710f52829c2ae81
Disha <thakuriyadisha@gmail.com>
2023-03-02 16:16:45 +0530

    Errors solved

--- blogwebsite/src/App.js
+++ blogwebsite/src/App.js
@@ -1,8 +1,10 @@
 import HomePage from "./Pages/Home/HomeCompo";
+import RouterComponent from "./Utility/RouteCompo";
 
 function App() {
   return (
     <div className="App">
+      <RouterComponent />
       <HomePage />
     </div>
   );
